f031c0a4c7821eb340631ba5ab32aaf6aa862552
Yechao Li <liyechao@yechao.xyz>
2023-02-16 20:19:11 +0800

    fix: add component api query template detail

--- api/src/routes/sdk.ts
+++ api/src/routes/sdk.ts
@@ -1,10 +1,11 @@
 import { component } from '@blocklet/sdk/lib/middlewares';
 import { Router } from 'express';
 
-import { getTemplates } from './templates';
+import { getTemplate, getTemplates } from './templates';
 
 const router = Router();
 
 router.get('/templates', component.verifySig, getTemplates);
+router.get('/templates/:templateId', component.verifySig, getTemplate);
 
 export default router;
