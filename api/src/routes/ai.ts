274547322f0f65405db9acde262fff14296c144d
Yechao LI <liyechao@yechao.xyz>
2023-03-23 14:42:46 +0800

    fix: use component `call` of @blocklet/sdk (#32)
    * fix: use component `call` of @blocklet/sdk
    
    * chore: update deps
    
    * chore: bump version

--- api/src/routes/ai.ts
+++ api/src/routes/ai.ts
@@ -1,41 +1,41 @@
+import { call } from '@blocklet/sdk/lib/component';
 import { Router } from 'express';
-
-import { proxyToComponent } from '../libs/component';
 
 const router = Router();
 
 router.get('/status', async (_, res) => {
-  await proxyToComponent(
-    {
-      name: 'ai-kit',
-      url: '/api/v1/sdk/status',
-    },
-    res
-  );
+  const response = await call({
+    name: 'ai-kit',
+    path: '/api/v1/sdk/status',
+    method: 'GET',
+    responseType: 'stream',
+  });
+  res.set('Content-Type', response.headers['content-type']);
+  response.data.pipe(res);
 });
 
 router.post('/completions', async (req, res) => {
-  await proxyToComponent(
-    {
-      name: 'ai-kit',
-      url: '/api/v1/sdk/completions',
-      method: 'POST',
-      data: req.body,
-    },
-    res
-  );
+  const response = await call({
+    name: 'ai-kit',
+    path: '/api/v1/sdk/completions',
+    method: 'POST',
+    data: req.body,
+    responseType: 'stream',
+  });
+  res.set('Content-Type', response.headers['content-type']);
+  response.data.pipe(res);
 });
 
 router.post('/image/generations', async (req, res) => {
-  await proxyToComponent(
-    {
-      name: 'ai-kit',
-      url: '/api/v1/sdk/image/generations',
-      method: 'POST',
-      data: req.body,
-    },
-    res
-  );
+  const response = await call({
+    name: 'ai-kit',
+    path: '/api/v1/sdk/image/generations',
+    method: 'POST',
+    data: req.body,
+    responseType: 'stream',
+  });
+  res.set('Content-Type', response.headers['content-type']);
+  response.data.pipe(res);
 });
 
 export default router;
