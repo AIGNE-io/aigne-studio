606c93e5f64bab8ef7c1e87041409ae9f2468c48
Yechao Li <liyechao@yechao.xyz>
2023-05-29 09:35:03 +0800

    fix: install hnswlib-node in pre-start

--- api/src/hooks/pre-start.ts
+++ api/src/hooks/pre-start.ts
@@ -1,5 +1,9 @@
 import '@blocklet/sdk/lib/error-handler';
 
+import { spawnSync } from 'child_process';
+import { chmodSync, existsSync, mkdirSync, symlinkSync } from 'fs';
+import { dirname, join } from 'path';
+
 import dotenv from 'dotenv-flow';
 
 import logger from '../libs/logger';
@@ -8,8 +12,45 @@ dotenv.config();
 
 const { name } = require('../../../package.json');
 
+const hnswlib = '@blocklet/hnswlib-node';
+
+async function ensureHNSWLIBBinaryFile() {
+  logger.info(`${name} ensure ${hnswlib}  installed`);
+
+  try {
+    await import(hnswlib);
+    logger.info(`${name} ${hnswlib} already installed`);
+    return;
+  } catch {
+    /* empty */
+  }
+  logger.info(`${name} try install ${hnswlib}`);
+
+  const appDir = process.env.BLOCKLET_APP_DIR!;
+
+  // link `node-pre-gyp` to .bin for download or build hnswlib-node
+  try {
+    const srcPath = join(appDir, 'node_modules/@mapbox/node-pre-gyp/bin/node-pre-gyp');
+    const binPath = join(appDir, 'node_modules/.bin/node-pre-gyp');
+    if (!existsSync(binPath) && existsSync(srcPath)) {
+      mkdirSync(dirname(binPath), { recursive: true });
+      symlinkSync(srcPath, binPath);
+      chmodSync(binPath, '755');
+    }
+  } catch (error) {
+    logger.warn(error.message);
+  }
+
+  spawnSync('npm', ['run', 'install'], {
+    cwd: join(appDir, `node_modules/${hnswlib}`),
+    stdio: 'inherit',
+    shell: true,
+  });
+}
+
 (async () => {
   try {
+    await ensureHNSWLIBBinaryFile();
     process.exit(0);
   } catch (err) {
     logger.error(`${name} pre-start error`, err.message);
