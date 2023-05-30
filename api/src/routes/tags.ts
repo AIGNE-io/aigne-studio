a47022ba698692400cfb8c0f534df0581d494049
Yechao Li <liyechao@yechao.xyz>
2023-05-30 18:31:47 +0800

    fix: support filter tags by template type

--- api/src/routes/tags.ts
+++ api/src/routes/tags.ts
@@ -2,24 +2,27 @@ import { Request, Response, Router } from 'express';
 import Joi from 'joi';
 
 import { ensureAdmin } from '../libs/security';
-import { tags } from '../store/tags';
+import { templates } from '../store/templates';
 
 const router = Router();
 
-const paginationSchema = Joi.object<{ offset: number; limit: number; sort?: string; search?: string }>({
-  offset: Joi.number().integer().min(0).default(0),
-  limit: Joi.number().integer().min(1).max(100).default(20),
+const paginationSchema = Joi.object<{ offset: number; limit: number; sort?: string; search?: string; type?: 'image' }>({
   search: Joi.string().empty(''),
+  type: Joi.string().valid('image').empty(''),
 });
 
 export async function getTags(req: Request, res: Response) {
-  const { offset, limit, ...query } = await paginationSchema.validateAsync(req.query);
-  const regex = query.search ? new RegExp(query.search, 'i') : undefined;
-  const filter = regex ? { $or: [{ name: { $regex: regex } }, { description: { $regex: regex } }] } : undefined;
+  const { search, type } = await paginationSchema.validateAsync(req.query, { stripUnknown: true });
 
-  const list = await tags.cursor(filter).sort({ updatedAt: -1 }).skip(offset).limit(limit).exec();
+  const filter = type ? { type } : undefined;
+  const list = await templates.cursor(filter).projection({ tags: 1 }).exec();
+  let tags = [...new Set(list.flatMap<string>((i) => i.tags ?? []))];
+  if (search) {
+    const regex = new RegExp(search, 'i');
+    tags = tags.filter((i) => regex.test(i));
+  }
 
-  res.json({ tags: list });
+  res.json({ tags: tags.map((name) => ({ name })) });
 }
 
 router.get('/', ensureAdmin, getTags);
