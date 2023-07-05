052259e79c26d4e469a3ed0d520b7cd073d84c06
Yechao LI <liyechao@yechao.xyz>
2023-07-05 11:39:26 +0800

    fix: correct join api url (#140)
    * fix: correct join api url
    
    * chore: update deps
    
    * chore: bump version

--- src/libs/ai.ts
+++ src/libs/ai.ts
@@ -1,5 +1,6 @@
 import { createImageGenerationApi, createStatusApi, createTextCompletionApi } from '@blocklet/ai-kit';
 import { fetchEventSource } from '@microsoft/fetch-event-source';
+import joinUrl from 'url-join';
 
 import { Template } from '../../api/src/store/templates';
 import axios from './api';
@@ -30,7 +31,7 @@ export async function callAI(
 
   return new ReadableStream<string | { type: 'text'; text: string } | { type: 'images'; images: { url: string }[] }>({
     async start(controller) {
-      await fetchEventSource(`${prefix}/api/ai/call`, {
+      await fetchEventSource(joinUrl(prefix, '/api/ai/call'), {
         openWhenHidden: true,
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
