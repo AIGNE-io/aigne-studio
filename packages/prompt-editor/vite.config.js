b9b28079084617e1ba7a4dea1b5f2bd9f4aaeec3
leermao <447552708@qq.com>
2023-09-26 14:09:46 +0800

    chore: yarn dev

--- packages/prompt-editor/vite.config.js
+++ packages/prompt-editor/vite.config.js
@@ -1,37 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import babel from '@rollup/plugin-babel';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { replaceCodePlugin } from 'vite-plugin-replace';
+import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    replaceCodePlugin({
-      replacements: [
-        {
-          from: /__DEV__/g,
-          to: 'true',
-        },
-      ],
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      babelrc: false,
-      configFile: false,
-      exclude: '/**/node_modules/**',
-      extensions: ['jsx', 'js', 'ts', 'tsx', 'mjs'],
-      presets: ['@babel/preset-react'],
-    }),
-    react(),
-  ],
-  build: {
-    outDir: 'build',
-    rollupOptions: {
-      input: {
-        main: new URL('./index.html', import.meta.url).pathname,
-        split: new URL('./split/index.html', import.meta.url).pathname,
-      },
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), svgr()],
+    build: {
+      chunkSizeWarningLimit: 2000,
     },
-  },
+  };
 });
