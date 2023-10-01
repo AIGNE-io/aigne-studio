4aa0e9d888f2a813be4c8a406094026c1d307b12
Yechao Li <liyechao@yechao.xyz>
2023-10-01 09:21:09 +0800

    fix: redirect to home if no authorization

--- blocklets/ai-studio/src/app.tsx
+++ blocklets/ai-studio/src/app.tsx
@@ -1,16 +1,16 @@
 import { LocaleProvider } from '@arcblock/ux/lib/Locale/context';
 import { ToastProvider } from '@arcblock/ux/lib/Toast';
+import { Dashboard } from '@blocklet/studio-ui';
 import Footer from '@blocklet/ui-react/lib/Footer';
-import Header from '@blocklet/ui-react/lib/Header';
 import { Box, CssBaseline, GlobalStyles, ThemeProvider, createTheme } from '@mui/material';
 import { ReactNode, Suspense, lazy, useEffect, useMemo, useRef } from 'react';
-import { BrowserRouter, Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import ErrorBoundary from './components/error/error-boundary';
 import Loading from './components/loading';
 import { DatasetsProvider } from './contexts/datasets';
-import { SessionProvider } from './contexts/session';
+import { SessionProvider, useInitialized, useIsPromptEditor } from './contexts/session';
 import { translations } from './locales';
 
 export default function App() {
@@ -65,42 +65,46 @@ function AppRoutes() {
     errorBoundary.current?.reset();
   }, [location]);
 
+  const initialized = useInitialized();
+  const isPromptEditor = useIsPromptEditor();
+
+  if (!initialized) return <Loading fixed />;
+
   return (
     <ErrorBoundary ref={errorBoundary}>
       <Routes>
-        <Route index element={<Navigate to="projects" replace />} />
-        <Route
-          path="*"
-          element={
-            <DatasetsProvider>
-              <Outlet />
-            </DatasetsProvider>
-          }>
-          <Route path="playground/*" element={<Navigate to="../projects" replace />} />
-          <Route path="projects/*" element={<ProjectsRoutes />} />
-          <Route path="datasets/*" element={<DatasetsRoutes />} />
-        </Route>
-        <Route path="/admin/resource/component/export" element={<ExportRoutes />} />
+        <Route index element={isPromptEditor ? <Navigate to="projects" replace /> : <Home />} />
+        {isPromptEditor ? (
+          <>
+            <Route path="playground/*" element={<Navigate to="../projects" replace />} />
+            <Route path="projects/*" element={<ProjectsRoutes />} />
+            <Route
+              path="datasets/*"
+              element={
+                <DatasetsProvider>
+                  <DatasetsRoutes />
+                </DatasetsProvider>
+              }
+            />
+            <Route path="/admin/resource/component/export" element={<ExportRoutes />} />
+          </>
+        ) : (
+          <Route path="*" element={<Navigate to="/" />} />
+        )}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </ErrorBoundary>
   );
 }
 
+const Home = lazy(() => import('./pages/home/home'));
+
 const ProjectsRoutes = lazy(() => import('./pages/projects'));
 
 const DatasetsRoutes = lazy(() => import('./pages/datasets'));
 
 function Layout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <Header maxWidth={null} />
-
-      {children}
-
-      <Footer />
-    </>
-  );
+  return <Dashboard footer={<Footer />}>{children}</Dashboard>;
 }
 
 function NotFound() {
