2a083d72dab35e7cb9604f0cbca59a01ee97f946
leermao <447552708@qq.com>
2023-10-07 18:52:22 +0800

    chore: code review

--- packages/prompt-editor/src/plugins/VariablePlugin/user-transform-node.tsx
+++ packages/prompt-editor/src/plugins/VariablePlugin/user-transform-node.tsx
@@ -1,4 +1,4 @@
-import { $createTextNode, LexicalEditor } from 'lexical';
+import { $createTextNode, LexicalEditor, TextNode } from 'lexical';
 import { useEffect } from 'react';
 
 import replaceNodes from './utils/replace-nodes';
@@ -6,7 +6,27 @@ import { hasBrackets, isBracketStartAndEnd } from './utils/util';
 import { $isVariableTextNode, VariableTextNode } from './variable-text-node';
 
 export default function useTransformVariableNode(editor: LexicalEditor) {
-  const nodeTransform = (node: VariableTextNode) => {
+  const nodeTextTransform = (node: TextNode) => {
+    const text = node.getTextContent();
+
+    if (hasBrackets(text)) {
+      if (!isBracketStartAndEnd(text)) {
+        replaceNodes({
+          fn: (_nodes) => {
+            _nodes.forEach((_node) => {
+              if ($isVariableTextNode(_node)) {
+                _node.select();
+              }
+            });
+          },
+          node,
+          text,
+        });
+      }
+    }
+  };
+
+  const nodeVariableTransform = (node: VariableTextNode) => {
     const text = node.getTextContent();
 
     if (hasBrackets(text)) {
@@ -29,9 +49,18 @@ export default function useTransformVariableNode(editor: LexicalEditor) {
   };
 
   useEffect(() => {
-    const handleTransform = editor.registerNodeTransform(VariableTextNode, nodeTransform);
+    const handleTransform = editor.registerNodeTransform(VariableTextNode, nodeVariableTransform);
     return () => {
       handleTransform();
     };
   }, [editor]);
+
+  useEffect(() => {
+    const handleTransform = editor.registerNodeTransform(TextNode, nodeTextTransform);
+    return () => {
+      handleTransform();
+    };
+  }, [editor]);
+
+  return null;
 }
