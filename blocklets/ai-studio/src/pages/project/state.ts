5ad2cfd181d1e8bf520f881165e702db2fe67ab1
leermao <447552708@qq.com>
2023-10-17 10:45:32 +0800

    fix: lint

--- blocklets/ai-studio/src/pages/project/state.ts
+++ blocklets/ai-studio/src/pages/project/state.ts
@@ -1,6 +1,7 @@
 import produce, { Draft } from 'immer';
 import { debounce, omit } from 'lodash';
 import { nanoid } from 'nanoid';
+import { ChatCompletionRequestMessage } from 'openai';
 import { useCallback } from 'react';
 import { RecoilState, atom, useRecoilState } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
@@ -295,7 +296,7 @@ export const useDebugState = ({ projectId, templateId }: { projectId: string; te
             ? await textCompletions({
                 stream: true,
                 messages: (
-                  (session?.messages.slice(-15).filter((i) => i.content) ?? []) as { role: string; content: string }[]
+                  (session?.messages.slice(-15).filter((i) => i.content) ?? []) as ChatCompletionRequestMessage[]
                 ).concat({
                   role: 'user',
                   content: message.content,
