2c562105cf67f355d1443b9801000010fa4adfbe
leermao <447552708@qq.com>
2023-10-23 11:27:49 +0800

    chore: support discuss view

---
+++ blocklets/ai-studio/src/pages/project/discuss-view.tsx
@@ -0,0 +1,33 @@
+import { Comments } from '@blocklet/discuss-kit';
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
+
+import { TemplateYjs } from '../../../api/src/store/projects';
+import { useSessionContext } from '../../contexts/session';
+
+export default function DiscussView({
+  projectId,
+  gitRef,
+  template,
+}: {
+  projectId: string;
+  gitRef: string;
+  template: TemplateYjs;
+}) {
+  const {
+    session: { user },
+  } = useSessionContext();
+
+  return (
+    <Views>
+      <Comments target={{ id: `${projectId}-${gitRef}-${template.id}`, owner: user.did }} displayReaction={false} />
+    </Views>
+  );
+}
+
+const Views = styled(Box)`
+  padding: 16px;
+  .comment-editor {
+    margin-top: 0;
+  }
+`;
