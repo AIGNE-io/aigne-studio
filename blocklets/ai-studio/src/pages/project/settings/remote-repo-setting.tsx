4ee76b2fd01d1711f5152e627884042c2428d3c4
Yechao Li <liyechao@yechao.xyz>
2023-11-17 09:56:39 +0800

    update link of github access token

--- blocklets/ai-studio/src/pages/project/settings/remote-repo-setting.tsx
+++ blocklets/ai-studio/src/pages/project/settings/remote-repo-setting.tsx
@@ -233,7 +233,7 @@ export default function RemoteRepoSetting({ projectId }: { projectId: string }) 
                       title={t('githubTokenTip')}
                       placement="top"
                       slotProps={{ popper: { sx: { whiteSpace: 'pre-wrap' } } }}>
-                      <Link href="https://github.com/settings/personal-access-tokens/new" target="_blank">
+                      <Link href="https://github.com/settings/tokens?type=beta" target="_blank">
                         github access token
                       </Link>
                     </Tooltip>
