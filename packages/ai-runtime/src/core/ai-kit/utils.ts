39aeca5998dfb4bd48e58b6256bcd4ebfb18430d
Yechao Li <liyechao@yechao.xyz>
2023-12-18 14:35:29 +0800

    fix: custom readableToWeb function instead of Readable.toWeb

--- packages/ai-runtime/src/core/ai-kit/utils.ts
+++ packages/ai-runtime/src/core/ai-kit/utils.ts
@@ -1,6 +1,18 @@
-import { TransformStream } from 'stream/web';
+import { Readable } from 'stream';
+import { ReadableStream, TransformStream } from 'stream/web';
 
 import { createParser } from 'eventsource-parser';
+
+export function readableToWeb(readable: Readable) {
+  return new ReadableStream({
+    async start(controller) {
+      for await (const chunk of readable) {
+        controller.enqueue(chunk);
+      }
+      controller.close();
+    },
+  });
+}
 
 export class EventSourceParserStream extends TransformStream<any, { data?: string }> {
   constructor() {
