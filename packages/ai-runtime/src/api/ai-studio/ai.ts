aea8db5514ba7b807417cf08e7fd1ccfe491b37c
Yechao LI <liyechao@yechao.xyz>
2023-12-27 12:02:49 +0800

    feat: export `callAssistant` api (#455)
    * feat: export callAssistant api
    
    * chore: bump version
    
    * fix: display tip if discuss kit not installed

---
+++ packages/ai-runtime/src/api/ai-studio/ai.ts
@@ -0,0 +1,122 @@
+import type { IncomingMessage } from 'http';
+import { ReadableStream, TextDecoderStream } from 'stream/web';
+
+import { EventSourceParserStream, readableToWeb } from '@blocklet/ai-kit/api/utils/event-stream';
+import { call } from '@blocklet/sdk/lib/component';
+import { AxiosResponse } from 'axios';
+
+import { RunAssistantChunk, RunAssistantResponse } from '../../core';
+import { isRunAssistantError } from '../assistant';
+
+export interface CallAssistantInput {
+  projectId: string;
+  ref: string;
+  working?: boolean;
+  assistantId: string;
+  parameters: { [key: string]: any };
+}
+
+export async function callAssistant(
+  input: CallAssistantInput,
+  options?: { responseType?: undefined }
+): Promise<{ content: string; images: RunAssistantChunk['delta']['images'] | null }>;
+export async function callAssistant(
+  input: CallAssistantInput,
+  options: { responseType: 'stream' }
+): Promise<AxiosResponse<IncomingMessage, any>>;
+export async function callAssistant(
+  input: CallAssistantInput,
+  options: { responseType: 'event-stream' }
+): Promise<ReadableStream<RunAssistantChunk>>;
+export async function callAssistant(
+  input: CallAssistantInput,
+  options: { responseType: 'text-stream' }
+): Promise<ReadableStream<string>>;
+export async function callAssistant(
+  input: CallAssistantInput,
+  options?: { responseType?: undefined | 'stream' | 'event-stream' | 'text-stream' }
+): Promise<
+  | { content: string; images: RunAssistantChunk['delta']['images'] | null }
+  | AxiosResponse<IncomingMessage, any>
+  | ReadableStream<RunAssistantChunk>
+  | ReadableStream<string>
+> {
+  const response = call({
+    name: 'ai-studio',
+    path: '/api/ai/call',
+    data: input,
+    headers: { Accept: 'text/event-stream' },
+    responseType: 'stream',
+  });
+
+  if (options?.responseType === 'stream') return response as any;
+
+  const stream = toEventStream((await response).data);
+
+  if (options?.responseType === 'event-stream') {
+    return stream;
+  }
+
+  if (options?.responseType === 'text-stream') {
+    return new ReadableStream<string>({
+      async start(controller) {
+        try {
+          let mainTaskId: string | undefined;
+
+          for await (const chunk of stream) {
+            mainTaskId ??= chunk.taskId;
+
+            if (mainTaskId === chunk.taskId) {
+              controller.enqueue(chunk.delta.content || '');
+            }
+          }
+        } catch (error) {
+          controller.error(error);
+        } finally {
+          controller.close();
+        }
+      },
+    });
+  }
+
+  let mainTaskId: string | undefined;
+  let content = '';
+  const images: NonNullable<RunAssistantChunk['delta']['images']> = [];
+
+  for await (const chunk of stream) {
+    mainTaskId ??= chunk.taskId;
+    if (mainTaskId === chunk.taskId) {
+      content += chunk.delta.content || '';
+      if (chunk.delta.images) images.push(...chunk.delta.images);
+    }
+  }
+
+  return {
+    content,
+    images: images.length ? images : null,
+  };
+}
+
+function toEventStream(data: IncomingMessage) {
+  return new ReadableStream<RunAssistantChunk>({
+    async start(controller) {
+      try {
+        const stream = readableToWeb(data)
+          .pipeThrough(new TextDecoderStream())
+          .pipeThrough(new EventSourceParserStream<RunAssistantResponse>());
+
+        for await (const chunk of stream) {
+          if (isRunAssistantError(chunk)) {
+            controller.error(new Error(chunk.error.message));
+            break;
+          }
+          controller.enqueue(chunk);
+        }
+      } catch (error) {
+        controller.error(error);
+      } finally {
+        controller.close();
+      }
+    },
+  });
+}
--- packages/ai-runtime/src/env.d.ts
+++ packages/ai-runtime/src/env.d.ts
@@ -1,13 +1,1 @@
-declare var blocklet:
-  | {
-      prefix: string;
-      appId: string;
-      appName: string;
-      appLogo: string;
-      appDescription: string;
-      version: string;
-      componentMountPoints: { title: string; name: string; did: string; mountPoint: string }[];
-    }
-  | undefined;
-
 declare module '@arcblock/ux/*';
