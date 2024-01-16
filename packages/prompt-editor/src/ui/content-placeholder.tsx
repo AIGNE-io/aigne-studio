653b252104b6005ad0aefcb6910d843e7eb7b86d
leermao <447552708@qq.com>
2024-01-16 20:14:59 +0800

    fix: Variables that target the execution block are not added to the variable

--- packages/prompt-editor/src/ui/content-placeholder.tsx
+++ packages/prompt-editor/src/ui/content-placeholder.tsx
@@ -4,14 +4,5 @@ import { Box } from '@mui/material';
 import { ReactNode } from 'react';
 
 export default function Placeholder({ children, className }: { children: ReactNode; className?: string }): JSX.Element {
-  return (
-    <Box
-      className={className || 'Placeholder__root'}
-      sx={{
-        fontSize: (theme) => theme.typography.caption.fontSize,
-        color: (theme) => theme.palette.action.disabled,
-      }}>
-      {children}
-    </Box>
-  );
+  return <Box className={className || 'Placeholder__root'}>{children}</Box>;
 }
