10f92f3c2529027a4b31a6bc93b07acee6577842
leermao <447552708@qq.com>
2024-05-15 17:06:21 +0800

    chore: add cron

--- blocklets/ai-studio/api/src/jobs/index.ts
+++ blocklets/ai-studio/api/src/jobs/index.ts
@@ -1,10 +1,13 @@
 import Cron from '@abtnode/cron';
+import config from '@blocklet/sdk/lib/config';
 
 import jobs from './jobs';
 
 let cronJob = null as any;
 
 const initCronJob = () => {
+  if (!config.env.preferences.autoUpdateKnowledge) return () => {};
+
   if (!cronJob) {
     cronJob = Cron.init({
       context: {},
--- blocklets/ai-studio/api/src/jobs/update-discuss-knowledge.ts
+++ blocklets/ai-studio/api/src/jobs/update-discuss-knowledge.ts
@@ -1,7 +1,11 @@
+import config from '@blocklet/sdk/lib/config';
+
 import { queue } from '../routes/dataset/embeddings';
 import DatasetDocument from '../store/models/dataset/document';
 
 const updateDiscussKnowledge = async () => {
+  if (!config.env.preferences.autoUpdateKnowledge) return;
+
   const documents = await DatasetDocument.findAll({
     where: { type: 'discussKit' },
   });
