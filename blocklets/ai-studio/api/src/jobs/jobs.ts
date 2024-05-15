ce94cfa5b211d35807520b0ff936b9c6b91daf29
leermao <447552708@qq.com>
2024-05-15 17:10:53 +0800

    chore: add cron

--- blocklets/ai-studio/api/src/jobs/jobs.ts
+++ blocklets/ai-studio/api/src/jobs/jobs.ts
@@ -3,7 +3,7 @@ import updateDiscussKnowledge from './update-discuss-knowledge';
 const jobs = [
   {
     name: 'update-discuss-kit-knowledge',
-    time: '0 * * * * *', // 每分钟
+    time: '0 0 * * * *', // 每小时更新
     fn: updateDiscussKnowledge,
     options: { runOnInit: true },
   },
