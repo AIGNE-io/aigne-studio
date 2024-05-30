206f5c29e23b89b13c3160215fc7d9be6cd4c92d
lynnlee0522 <386984469@qq.com>
2024-05-30 15:02:05 +0800

    simplify agent tooltip

--- blocklets/ai-studio/src/components/agent-tooltip/index.tsx
+++ blocklets/ai-studio/src/components/agent-tooltip/index.tsx
@@ -1,4 +1,4 @@
-import { Theme, Tooltip, TooltipProps, useMediaQuery } from '@mui/material';
+import { Box, Theme, Tooltip, TooltipProps, useMediaQuery } from '@mui/material';
 import { ReactElement } from 'react';
 
 export function AgentTooltip(props: { children: React.ReactElement<any, any> } & TooltipProps): ReactElement {
@@ -7,13 +7,15 @@ export function AgentTooltip(props: { children: React.ReactElement<any, any> } &
   // https://github.com/mui/material-ui/issues/14948
   // 根据官方建议，移动端使用 Tooltip 组件的时候，
   // 使用 enterTouchDelay 和 leaveTouchDelay 来控制 Tooltip 显示和隐藏
-  function MoblieTooltip() {
-    return (
-      <Tooltip {...props} enterTouchDelay={0} leaveTouchDelay={1000}>
-        {props.children}
-      </Tooltip>
-    );
-  }
-
-  return <> {isMobile ? MoblieTooltip() : <Tooltip {...props}>{props.children}</Tooltip>} </>;
+  return (
+    <Box>
+      {isMobile ? (
+        <Tooltip enterTouchDelay={0} leaveTouchDelay={3000} {...props}>
+          {props.children}
+        </Tooltip>
+      ) : (
+        <Tooltip {...props}>{props.children}</Tooltip>
+      )}
+    </Box>
+  );
 }
