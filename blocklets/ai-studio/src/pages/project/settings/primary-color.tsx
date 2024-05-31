9959387a28b5a7c9de0d75460b73461c8788691c
Yechao Li <liyechao@yechao.xyz>
2024-05-31 08:08:22 +0800

    fix: remove duplicated title from settings

--- blocklets/ai-studio/src/pages/project/settings/primary-color.tsx
+++ blocklets/ai-studio/src/pages/project/settings/primary-color.tsx
@@ -1,7 +1,6 @@
 import { UpdateProjectInput } from '@api/routes/project';
-import { useLocaleContext } from '@arcblock/ux/lib/Locale/context';
 import { ClickAwayListener } from '@mui/base';
-import { Box, Divider, Popper, Stack, Typography, styled } from '@mui/material';
+import { Box, Divider, Popper, Stack, styled } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { ChromePicker } from 'react-color';
 
@@ -17,7 +16,7 @@ const defaultColors: string[] = [
   '#ba68c8',
 ];
 
-export default function PrimayColor({
+export default function PrimaryColor({
   value,
   set,
 }: {
@@ -27,7 +26,6 @@ export default function PrimayColor({
   const [selectedColor, setSelectedColor] = useState<string | undefined>(
     value.appearance?.primaryColor || defaultColors[0]
   );
-  const { t } = useLocaleContext();
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const [open, setOpen] = useState(false);
 
@@ -48,34 +46,29 @@ export default function PrimayColor({
   }, [selectedColor]);
 
   return (
-    <Box>
-      <Typography variant="subtitle2" mb={0.5}>
-        {t('primaryColor')}
-      </Typography>
-      <Stack gap={2}>
-        <Stack direction="row" gap={1} alignItems="center">
-          {defaultColors?.map((color) => (
-            <Box key={color} border={color === selectedColor ? '1px solid #030712' : ''} borderRadius="4px">
-              <ColorBox bgcolor={color} onClick={() => handleClick(color)} />
-            </Box>
-          ))}
+    <Stack gap={2}>
+      <Stack direction="row" gap={1} alignItems="center">
+        {defaultColors?.map((color) => (
+          <Box key={color} border={color === selectedColor ? '1px solid #030712' : ''} borderRadius="4px">
+            <ColorBox bgcolor={color} onClick={() => handleClick(color)} />
+          </Box>
+        ))}
 
-          <Divider orientation="vertical" flexItem sx={{ mx: '2px' }} />
-          <ClickAwayListener onClickAway={() => setOpen(false)}>
-            <Box>
-              <Box onClick={handleOpen}>
-                <ChromePickerBox bgcolor={selectedColor} />
-              </Box>
-              <Popper open={open} anchorEl={anchorEl} sx={{ zIndex: 10000 }} placement="bottom-end">
-                <Box mt={2}>
-                  <ChromePicker onChangeComplete={handleChangeComplete} color={selectedColor} disableAlpha />
-                </Box>
-              </Popper>
+        <Divider orientation="vertical" flexItem sx={{ mx: '2px' }} />
+        <ClickAwayListener onClickAway={() => setOpen(false)}>
+          <Box>
+            <Box onClick={handleOpen}>
+              <ChromePickerBox bgcolor={selectedColor} />
             </Box>
-          </ClickAwayListener>
-        </Stack>
+            <Popper open={open} anchorEl={anchorEl} sx={{ zIndex: 10000 }} placement="bottom-end">
+              <Box mt={2}>
+                <ChromePicker onChangeComplete={handleChangeComplete} color={selectedColor} disableAlpha />
+              </Box>
+            </Popper>
+          </Box>
+        </ClickAwayListener>
       </Stack>
-    </Box>
+    </Stack>
   );
 }
 
