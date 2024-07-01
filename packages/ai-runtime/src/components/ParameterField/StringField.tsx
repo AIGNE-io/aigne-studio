518cb710e83a93b77cdfeca19fb8d938dfd62a7c
Yechao Li <liyechao@yechao.xyz>
2024-07-02 00:09:23 +0800

    chore: support multiline text for question input

--- packages/ai-runtime/src/components/ParameterField/StringField.tsx
+++ packages/ai-runtime/src/components/ParameterField/StringField.tsx
@@ -1,6 +1,6 @@
 import { TextField, TextFieldProps } from '@mui/material';
 import { pick } from 'lodash';
-import { forwardRef } from 'react';
+import { KeyboardEvent, forwardRef, useCallback } from 'react';
 
 import { StringParameter } from '../../types/assistant';
 
@@ -12,13 +12,31 @@ const StringField = forwardRef<
     onChange: (value: string) => void;
   } & Omit<TextFieldProps, 'onChange'>
 >(({ readOnly, parameter, onChange, ...props }, ref) => {
+  const isQuestion = parameter?.key === 'question';
+
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (!isQuestion) return;
+
+      // NOTE: Pressing Enter in the IME will trigger the 229 event
+      if (e.keyCode === 229) return;
+      if (e.key === 'Enter' && !e.shiftKey && !e.repeat) {
+        e.preventDefault();
+        (e.target as HTMLInputElement).form?.requestSubmit();
+      }
+    },
+    [isQuestion]
+  );
+
   return (
     <TextField
       ref={ref}
-      {...pick(parameter, 'required', 'label', 'placeholder', 'multiline')}
+      {...pick(parameter, 'required', 'label', 'placeholder')}
       helperText={parameter?.helper}
-      minRows={parameter?.multiline ? 2 : undefined}
+      multiline={isQuestion || parameter?.multiline || isQuestion}
+      minRows={isQuestion ? 1 : parameter?.multiline ? 2 : undefined}
       onChange={(e) => onChange(e.target.value)}
+      onKeyDown={onKeyDown}
       {...props}
       InputProps={{
         ...props.InputProps,
