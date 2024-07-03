a4803b0b159d82a3b3c4925a284dd754e16f4bdb
Yechao Li <liyechao@yechao.xyz>
2024-07-03 14:15:53 +0800

    chore: update deps

--- blocklets/ai-runtime/src/pages/preview.tsx
+++ blocklets/ai-runtime/src/pages/preview.tsx
@@ -2,8 +2,7 @@ import ApplicationHeader from '@app/components/application/ApplicationHeader';
 import Result from '@arcblock/ux/lib/Result';
 import { getAgent } from '@blocklet/aigne-sdk/api/agent';
 import AgentView from '@blocklet/aigne-sdk/components/AgentView';
-import { useCurrentMessage } from '@blocklet/aigne-sdk/components/ai-runtime';
-import { Box, Button, CircularProgress } from '@mui/material';
+import { Box, CircularProgress } from '@mui/material';
 import { useRequest } from 'ahooks';
 import { useParams } from 'react-router-dom';
 
@@ -19,14 +18,7 @@ export default function PreviewPage() {
       <ApplicationHeader application={data && { aid, project: data.project }} />
 
       {data ? (
-        <AgentView
-          aid={aid}
-          working
-          hideInputFields={['question']}
-          initialInputValues={{ question: 'Hello' }}
-          autoGenerate
-          customOutputActionsComponent={CustomActions}
-        />
+        <AgentView aid={aid} working />
       ) : loading ? (
         <Box textAlign="center" my={10}>
           <CircularProgress size={24} />
@@ -35,16 +27,5 @@ export default function PreviewPage() {
         <Box component={Result} status={404} sx={{ bgcolor: 'transparent', my: 10 }} />
       )}
     </>
-  );
-}
-
-function CustomActions() {
-  const message = useCurrentMessage();
-  console.log('AIGNE Runtime preview', { message });
-
-  return (
-    <Box>
-      <Button>COPY</Button>
-    </Box>
   );
 }
