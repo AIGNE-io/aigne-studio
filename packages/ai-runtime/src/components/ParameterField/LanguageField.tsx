0ae5fe76cb2bf919266e471cc93c135de0fc12cb
lynnlee0522 <lilin@arcblock.io>
2024-07-11 18:28:16 +0800

    chore: using a language selector with improved interactivity (#1208)
    * chore: Replaced with a language selector with enhanced interactivity

--- packages/ai-runtime/src/components/ParameterField/LanguageField.tsx
+++ packages/ai-runtime/src/components/ParameterField/LanguageField.tsx
@@ -1,9 +1,30 @@
 import { useLocaleContext } from '@arcblock/ux/lib/Locale/context';
-import { MenuItem, TextField, TextFieldProps } from '@mui/material';
+import { Autocomplete, ListItem, ListItemIcon, ListItemText, MenuItem, TextField, TextFieldProps } from '@mui/material';
 import { forwardRef } from 'react';
 
 import { languages } from '../../constant/languages';
 import { SelectParameter } from '../../types/assistant';
+
+type Option = {
+  name: string;
+  en: string;
+  cn: string;
+  abbr: string;
+  flag: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
+};
+
+const filter = (options: Option[], state: { inputValue: string }) => {
+  return options.filter((o: Option) => {
+    if (state.inputValue) {
+      return (
+        o.en.toLowerCase().includes(state.inputValue.toLowerCase()) ||
+        o.name.toLowerCase().includes(state.inputValue.toLowerCase())
+      );
+    }
+
+    return true;
+  });
+};
 
 const LanguageField = forwardRef<
   HTMLDivElement,
@@ -13,38 +34,62 @@ const LanguageField = forwardRef<
     onChange: (value: string | undefined) => void;
   } & Omit<TextFieldProps, 'onChange'>
 >(({ readOnly, parameter, onChange, ...props }, ref) => {
+  const value = props?.value ? languages.find((o) => o.en === props.value) : null;
   const { locale } = useLocaleContext();
 
   return (
-    <TextField
-      ref={ref}
-      required={parameter?.required}
-      label={parameter?.label}
-      placeholder={parameter?.placeholder}
-      helperText={parameter?.helper}
-      select
-      onChange={(e) => onChange(e.target.value)}
-      {...props}
-      InputProps={{ ...props.InputProps, readOnly }}
-      sx={{
-        ...props.sx,
-        '& .MuiSelect-select .notranslate::after': parameter?.placeholder
-          ? {
-              content: `"${parameter.placeholder}"`,
-              opacity: 0.42,
-            }
-          : {},
-        '& .MuiFormLabel-root:not(.MuiInputLabel-shrink) + .MuiInputBase-root > .MuiSelect-select .notranslate::after':
-          {
-            opacity: 0,
-          },
-      }}>
-      {languages.map((option) => (
-        <MenuItem key={option.en} value={option.en}>
-          {locale === 'zh' ? option.cn : option.en}
-        </MenuItem>
-      ))}
-    </TextField>
+    <Autocomplete
+      options={languages}
+      renderInput={(params) => (
+        <TextField
+          ref={ref}
+          required={parameter?.required}
+          label={parameter?.label}
+          placeholder={parameter?.placeholder}
+          helperText={parameter?.helper}
+          {...params}
+          {...props}
+          sx={{
+            ...props.sx,
+            '& .MuiSelect-select .notranslate::after': parameter?.placeholder
+              ? {
+                  content: `"${parameter.placeholder}"`,
+                  opacity: 0.42,
+                }
+              : {},
+            '& .MuiFormLabel-root:not(.MuiInputLabel-shrink) + .MuiInputBase-root > .MuiSelect-select .notranslate::after':
+              {
+                opacity: 0,
+              },
+          }}
+        />
+      )}
+      getOptionLabel={(o: Option) => {
+        return locale === 'zh' ? o.cn : o.en;
+      }}
+      getOptionKey={(i) => i.en}
+      value={value}
+      isOptionEqualToValue={(option, value) => {
+        if (value === null) return false;
+        return option?.en === value?.en;
+      }}
+      onChange={(_e, newValue) => {
+        onChange?.(newValue?.en);
+      }}
+      renderOption={(props, option) => {
+        return (
+          <MenuItem {...props}>
+            <ListItem sx={{ py: 0 }}>
+              <ListItemIcon>
+                <option.flag />
+              </ListItemIcon>
+              <ListItemText primary={`${option.name} ${locale === 'zh' ? option.cn : option.en} (${option.abbr})`} />
+            </ListItem>
+          </MenuItem>
+        );
+      }}
+      filterOptions={filter}
+    />
   );
 });
 
