eac9d22f1759762e295e0e41904dd13263fd62c7
Yechao Li <liyechao@yechao.xyz>
2024-08-08 11:08:05 +0800

    chore: simplify render message logic

--- packages/ai-runtime/src/core/utils/render-message.ts
+++ packages/ai-runtime/src/core/utils/render-message.ts
@@ -2,32 +2,24 @@ import { get } from 'lodash';
 
 import { Mustache } from '../../types/assistant';
 
-const isBracketStartAndEnd = (text: string) => {
-  const pattern = /^\{\{(.*)\}\}$/;
-  const match = text.match(pattern);
-  return !!match;
-};
-
 // 调用地方:
 // Input
 // process
 // ReadableStream => 需要使用到 .slice(1, -1), 我看只有这一个地方使用到了, 是否需要将 slice 的逻辑提取到业务中?
 export async function renderMessage(message: string, parameters?: { [key: string]: any }, stringify = true) {
-  const fn = async () => {
-    const spans = Mustache.parse(message);
-    if (spans.length === 1 && isBracketStartAndEnd(message.trim())) {
-      const span = spans[0]?.[1];
-      if (span) return get(parameters, span);
+  const spans = Mustache.parse(message.trim());
+  if (spans.length === 1) {
+    const span = spans[0]!;
+    if (span[0] === 'name') {
+      const result = get(parameters, span[1]);
+      return stringify && typeof result !== 'string' ? JSON.stringify(result) : result;
     }
+  }
 
-    return Mustache.render(message, parameters, undefined, {
-      escape: (v) => {
-        const r = typeof v === 'object' ? JSON.stringify(v) : v;
-        return typeof r === 'string' ? JSON.stringify(r).slice(1, -1) : r;
-      },
-    });
-  };
-
-  const result = await fn();
-  return stringify ? (typeof result === 'object' ? JSON.stringify(result) : result) : result;
+  return Mustache.render(message, parameters, undefined, {
+    escape: (v) => {
+      const r = typeof v === 'object' ? JSON.stringify(v) : v;
+      return typeof r === 'string' ? JSON.stringify(r).slice(1, -1) : r;
+    },
+  });
 }
