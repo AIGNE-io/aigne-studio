af64cc16060165109ddb72f1a4c90796fe579f92
leermao <447552708@qq.com>
2024-08-09 08:47:53 +0800

    chore: code review

--- packages/ai-runtime/src/core/utils/render-message.ts
+++ packages/ai-runtime/src/core/utils/render-message.ts
@@ -17,21 +17,19 @@ export async function renderMessage(
   parameters?: { [key: string]: any },
   options: { stringify: boolean } = { stringify: true }
 ) {
-  const fn = async () => {
-    const spans = Mustache.parse(message);
-    if (spans.length === 1 && onlyOneVariable(message.trim())) {
-      const span = (spans[0]?.[1] || '').trim();
-      if (span) return get(parameters, span);
+  const spans = Mustache.parse(message.trim());
+  if (spans.length === 1 && onlyOneVariable(message.trim())) {
+    const span = spans[0]!;
+    if (span[0] === 'name') {
+      const result = get(parameters, span[1]);
+      return options.stringify && typeof result !== 'string' ? JSON.stringify(result) : result;
     }
+  }
 
-    return Mustache.render(message, parameters, undefined, {
-      escape: (v) => {
-        const r = typeof v === 'object' ? JSON.stringify(v) : v;
-        return typeof r === 'string' ? JSON.stringify(r).slice(1, -1) : r;
-      },
-    });
-  };
-
-  const result = await fn();
-  return options?.stringify ? (typeof result === 'object' ? JSON.stringify(result) : result) : result;
+  return Mustache.render(message, parameters, undefined, {
+    escape: (v) => {
+      const r = typeof v === 'object' ? JSON.stringify(v) : v;
+      return typeof r === 'string' ? JSON.stringify(r).slice(1, -1) : r;
+    },
+  });
 }
