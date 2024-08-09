faeb40ad3b59a383ae916921c0b0daf644364035
leermao <447552708@qq.com>
2024-08-09 17:47:55 +0800

    chore: add vim

--- packages/code-editor/src/code-editor.tsx
+++ packages/code-editor/src/code-editor.tsx
@@ -1,7 +1,15 @@
+import { cx } from '@emotion/css';
+import { Icon } from '@iconify-icon/react';
+import FullMaxIcon from '@iconify-icons/tabler/arrows-maximize';
+import FullMinIcon from '@iconify-icons/tabler/arrows-minimize';
+import SettingIcon from '@iconify-icons/tabler/settings';
 import Editor, { useMonaco } from '@monaco-editor/react';
-import { Box, BoxProps, useTheme } from '@mui/material';
+import { Box, BoxProps, IconButton, styled, useTheme } from '@mui/material';
+import useLocalStorageState from 'ahooks/lib/useLocalStorageState';
+import { editor } from 'monaco-editor';
 import { customAlphabet } from 'nanoid';
-import { forwardRef, useEffect, useImperativeHandle } from 'react';
+import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
 
 const randomId = customAlphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
 const id = randomId();
@@ -58,34 +66,73 @@ function setupMonaco(monaco: typeof import('monaco-editor')) {
       'editor.background': '#ffffff',
     },
   });
+
   monaco.editor.setTheme(themeName);
 }
 
 const CodeEditor = forwardRef(
   ({ readOnly, maxHeight, ...props }: { readOnly?: boolean; maxHeight?: number } & BoxProps<typeof Editor>, ref) => {
+    const [editor, setEditor] = useState<ReturnType<(typeof import('monaco-editor'))['editor']['create']>>();
     const monaco = useMonaco();
+    const theme = useTheme();
+    const handle = useFullScreenHandle();
+    const [settings, setSettings] = useLocalStorageState<{ vim: boolean; vimMode?: 'insert' | 'normal' }>(
+      `editor-${id}`,
+      { defaultValue: { vim: false, vimMode: undefined } }
+    );
+
+    const statusRef = useRef<HTMLElement>(null);
+    const vimModeRef = useRef<any>();
 
     useEffect(() => {
-      if (monaco) {
-        setupMonaco(monaco);
-      }
+      if (monaco) setupMonaco(monaco);
     }, [monaco]);
+
+    useEffect(() => {
+      if (settings?.vim) {
+        if (editor) {
+          import('monaco-vim').then(({ initVimMode }) => {
+            vimModeRef.current = initVimMode(editor, statusRef.current!);
+            setSettings((r) => ({ ...r!, vimMode: 'normal' }));
+            vimModeRef.current.on('vim-mode-change', ({ mode }: any) => setSettings((r) => ({ ...r!, vimMode: mode })));
+          });
+        }
+      } else {
+        vimModeRef.current?.dispose();
+      }
+    }, [settings?.vim, editor]);
+
+    useEffect(() => {
+      return () => vimModeRef.current?.dispose();
+    }, []);
 
     useImperativeHandle(ref, () => {});
 
-    const theme = useTheme();
-
     return (
-      <Box sx={{ position: 'relative', width: 1, height: 1 }}>
+      <Full handle={handle}>
         <Box
+          className={cx(props.className, settings?.vimMode === 'normal' && 'vim-normal')}
           component={Editor}
           theme={themeName}
           {...props}
           sx={{
+            '.monaco-editor:focus': {
+              outline: 'none !important',
+            },
             '--vscode-menu-background': 'rgba(255,255,255,1)',
             '--vscode-widget-shadow': 'rgba(0,0,0,0.1)',
             '.overflowingContentWidgets': { position: 'relative', zIndex: theme.zIndex.tooltip },
             ...props.sx,
+            '&.vim-normal .cursor.monaco-mouse-cursor-text':
+              settings?.vimMode === 'normal'
+                ? {
+                    width: '9px !important',
+                    backgroundColor: '#aeafad',
+                    borderColor: '#aeafad',
+                    color: '#515052',
+                    opacity: 0.7,
+                  }
+                : {},
           }}
           options={{
             lineNumbersMinChars: 2,
@@ -104,10 +151,45 @@ const CodeEditor = forwardRef(
               ...props.options?.scrollbar,
             },
           }}
+          onMount={(editor: editor.IStandaloneCodeEditor) => {
+            setEditor(editor);
+          }}
         />
-      </Box>
+
+        <Box sx={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}>
+          <Box ref={statusRef} sx={{ fontSize: 12, color: 'action.active' }} />
+        </Box>
+
+        <Box
+          className="settings"
+          sx={{ position: 'absolute', right: 20, top: 0, zIndex: 1, display: 'none', alignItems: 'center', gap: 0.5 }}>
+          <IconButton size="small" onClick={handle.active ? handle.exit : handle.enter}>
+            <Box
+              component={Icon}
+              icon={handle.active ? FullMinIcon : FullMaxIcon}
+              sx={{ color: 'action.active', fontSize: 20 }}
+            />
+          </IconButton>
+
+          <IconButton size="small" onClick={() => setSettings((r) => ({ ...r!, vim: !r?.vim }))}>
+            <Box component={Icon} icon={SettingIcon} sx={{ color: 'action.active', fontSize: 20 }} />
+          </IconButton>
+        </Box>
+      </Full>
     );
   }
 );
+
+const Full = styled(FullScreen)`
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  &:hover {
+    .settings {
+      display: flex;
+    }
+  }
+`;
 
 export default CodeEditor;
