edfb64e9f032b00b38470b6d4ffd480b23d52b15
Yechao Li <liyechao@yechao.xyz>
2024-08-26 16:07:46 +0800

    fix: skip xss middleware for some internal api

---
+++ blocklets/ai-studio/api/src/libs/xss.ts
@@ -0,0 +1,22 @@
+import { xss as xssMiddleware } from '@blocklet/xss';
+import { NextFunction, Request, Response } from 'express';
+
+const SKIP_XSS_API: { method?: string; path: string | RegExp }[] = [{ path: /^\/api\/ai/ }];
+
+export function xss() {
+  const middleware = xssMiddleware();
+
+  return (req: Request, res: Response, next: NextFunction) => {
+    const skip = SKIP_XSS_API.some(
+      (i) =>
+        (!i.method || i.method.toLowerCase() === req.method.toLowerCase()) &&
+        (i.path instanceof RegExp ? i.path.test(req.path) : i.path === req.path)
+    );
+
+    if (skip) {
+      next();
+    } else {
+      middleware(req, res, next);
+    }
+  };
+}
