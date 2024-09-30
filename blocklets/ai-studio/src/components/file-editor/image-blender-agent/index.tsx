9afe7dda8f977fc15846f5974a023bb7061c80ab
Yongzhuo Liang <firetable@foxmail.com>
2024-09-30 10:39:58 +0800

    chore: add Select Templates component

--- blocklets/ai-studio/src/components/file-editor/image-blender-agent/index.tsx
+++ blocklets/ai-studio/src/components/file-editor/image-blender-agent/index.tsx
@@ -1,6 +1,12 @@
 import { ImageBlenderAssistantYjs } from '@blocklet/ai-runtime/types';
-import { Map, getYjsValue } from '@blocklet/co-git/yjs';
-import { Stack, TextField, Typography } from '@mui/material';
+// import { Map, getYjsValue } from '@blocklet/co-git/yjs';
+import { Box, TextField, Typography } from '@mui/material';
+import { SelectTemplates } from '@nft-studio/react';
+import { useRef } from 'react';
+
+function CustomTextField(props: any) {
+  return <TextField {...props} InputProps={{ startAdornment: null, endAdornment: props?.InputProps?.endAdornment }} />;
+}
 
 export default function ImageBlenderAssistantEditor({
   value,
@@ -9,36 +15,54 @@ export default function ImageBlenderAssistantEditor({
   value: ImageBlenderAssistantYjs;
   disabled?: boolean;
 }) {
-  const dynamicDataKeys = ['text', 'qrcode', 'url'];
-  const doc = (getYjsValue(value) as Map<any>).doc!;
-
-  const setDynamicData = (key: string, val: string) => {
-    doc.transact(() => {
-      value.dynamicData ??= {};
-      value.dynamicData[key] = val;
-    });
-  };
+  // const doc = (getYjsValue(value) as Map<any>).doc!;
+  const selectedTemplatesRef = useRef({});
 
   return (
-    <Stack gap={2.5}>
-      <TextField
-        disabled={disabled}
-        label="templateId"
-        value={value.templateId || ''}
-        onChange={(e) => (value.templateId = e.target.value)}
+    <Box
+      sx={{
+        backgroundColor: 'white',
+        pt: 2,
+        borderRadius: 1,
+        // overflow: 'hidden',
+      }}>
+      <SelectTemplates
+        ref={selectedTemplatesRef}
+        // @ts-ignore
+        initialValue={
+          value.templateId && value.dynamicData
+            ? [
+                {
+                  _id: value.templateId,
+                  templateId: value.templateId,
+                  dynamicData: value.dynamicData,
+                },
+              ]
+            : null
+        }
+        // @ts-ignore
+        onChange={(templates: any[]) => {
+          const [currentTemplate] = templates;
+          value.templateId = currentTemplate?.templateId;
+          value.dynamicData = currentTemplate?.dynamicData;
+        }}
+        slots={{
+          ConfigMode: {
+            top: (props: any) => (
+              <Box
+                sx={{
+                  width: '100%',
+                  mb: 2,
+                }}>
+                <Typography sx={{ fontSize: 16, fontWeight: 600, mb: 1 }}>SN</Typography>
+                <CustomTextField {...props} disabled={disabled} size="small" label="Sn" />
+              </Box>
+            ),
+          },
+          TextField: CustomTextField,
+        }}
+        sx={{ height: '100%', borderRadius: 2 }}
       />
-
-      <Typography>Dynamic Data</Typography>
-
-      {dynamicDataKeys.map((key) => (
-        <TextField
-          disabled={disabled}
-          key={key}
-          label={key}
-          value={value.dynamicData?.[key]}
-          onChange={(e) => setDynamicData(key, e.target.value)}
-        />
-      ))}
-    </Stack>
+    </Box>
   );
 }
