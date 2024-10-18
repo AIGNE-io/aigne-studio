87824e6b701ad0669b3f2ac0748e3a29e03555d7
donotlb <donotlb@gmail.com>
2024-10-18 19:58:22 +0800

    tweaks

--- packages/aigne-sdk/src/api/premium.ts
+++ packages/aigne-sdk/src/api/premium.ts
@@ -1,9 +1,6 @@
 import config from '@blocklet/sdk/lib/config';
-import AuthService from '@blocklet/sdk/lib/service/auth';
 
 import { Quotas } from '../quotas';
-
-export const authClient = new AuthService();
 
 export const quotaChecker = new Quotas(config.env.preferences.quotas);
 
