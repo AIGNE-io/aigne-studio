86b170bada38ee3a26693b3f50ebd7de28b435df
donotlb <donotlb@gmail.com>
2024-10-31 17:36:40 +0800

    add db indexes

--- blocklets/ai-runtime/api/src/store/migrations/20241030170000-add-history-request-type.ts
+++ blocklets/ai-runtime/api/src/store/migrations/20241030170000-add-history-request-type.ts
@@ -6,9 +6,15 @@ import type { Migration } from '../migrate';
 export const up: Migration = async ({ context: queryInterface }) => {
   await queryInterface.addColumn('Histories', 'requestType', { type: DataTypes.STRING });
   await queryInterface.addColumn('Histories', 'projectOwnerId', { type: DataTypes.STRING });
+
+  await queryInterface.addIndex('Histories', ['requestType']);
+  await queryInterface.addIndex('Histories', ['projectOwnerId']);
 };
 
 export const down: Migration = async ({ context: queryInterface }) => {
   await queryInterface.removeColumn('Histories', 'requestType');
   await queryInterface.removeColumn('Histories', 'projectOwnerId');
+
+  await queryInterface.removeIndex('Histories', ['requestType']);
+  await queryInterface.removeIndex('Histories', ['projectOwnerId']);
 };
