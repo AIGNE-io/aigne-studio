a0f45a55800c8980664c7fbd9ac40f511d2e5ee8
july <july@test.com>
2024-11-04 17:51:22 +0800

    fix(ai-studio): space card action

--- blocklets/ai-studio/src/pages/project/settings/did-spaces-setting.tsx
+++ blocklets/ai-studio/src/pages/project/settings/did-spaces-setting.tsx
@@ -1,28 +1,23 @@
 import { useSessionContext } from '@app/contexts/session';
 import { getDefaultBranch } from '@app/store/current-git-store';
 import { useLocaleContext } from '@arcblock/ux/lib/Locale/context';
-import RelativeTime from '@arcblock/ux/lib/RelativeTime';
 import Toast from '@arcblock/ux/lib/Toast';
 import { SpaceCard } from '@blocklet/did-space-react';
-import { CheckCircleOutlineRounded, ErrorOutlineRounded, SyncRounded } from '@mui/icons-material';
+import { CheckCircleOutlineRounded, ErrorOutlineRounded } from '@mui/icons-material';
 import { CircularProgress, FormControlLabel, Stack, Typography } from '@mui/material';
 import { useCallback, useState } from 'react';
 
 import Switch from '../../../components/custom/switch';
-import PromiseLoadingButton from '../../../components/promise-loading-button';
 import { getErrorMessage } from '../../../libs/api';
-import { isTheErrorShouldShowMergeConflict, useMergeConflictDialog } from '../save-button';
 import { useProjectState } from '../state';
 import GatewayAction from './gateway-action';
 
 export default function DidSpacesSetting({ projectId }: { projectId: string }) {
-  const { t, locale } = useLocaleContext();
-  const { showMergeConflictDialog } = useMergeConflictDialog({ projectId });
-  const { state, updateProject, sync } = useProjectState(projectId, getDefaultBranch());
+  const { t } = useLocaleContext();
+  const { state, updateProject } = useProjectState(projectId, getDefaultBranch());
   const { session } = useSessionContext();
 
   const [authSyncUpdating, setAutoSyncUpdating] = useState<boolean | 'success' | 'error'>(false);
-  const [refreshSpaceCard, setRefreshSpaceCard] = useState(false);
 
   const changeDidSpaceAutoSync = useCallback(
     async (didSpaceAutoSync: boolean) => {
@@ -43,7 +38,7 @@ export default function DidSpacesSetting({ projectId }: { projectId: string }) {
     <Stack gap={2.5}>
       <Typography variant="subtitle2">{t('didSpaces.title')}</Typography>
 
-      <Stack direction="row" alignItems="center" gap={1}>
+      <Stack direction="row" alignItems="center" justifyContent="flex-end" gap={1}>
         <Stack direction="row" alignItems="center" justifyContent="space-between" gap={1}>
           <FormControlLabel
             sx={{ m: 0 }}
@@ -69,36 +64,6 @@ export default function DidSpacesSetting({ projectId }: { projectId: string }) {
             </Stack>
           ) : null}
         </Stack>
-
-        <Stack direction="row" alignItems="center" justifyContent="space-between" gap={1}>
-          <PromiseLoadingButton
-            size="small"
-            loadingPosition="start"
-            startIcon={<SyncRounded />}
-            onClick={async () => {
-              try {
-                await sync(projectId, 'didSpace');
-                Toast.success(t('synced'));
-              } catch (error) {
-                if (isTheErrorShouldShowMergeConflict(error)) {
-                  showMergeConflictDialog();
-                  return;
-                }
-                Toast.error(getErrorMessage(error));
-              } finally {
-                setRefreshSpaceCard((v) => !v);
-              }
-            }}>
-            {t('sync')}
-          </PromiseLoadingButton>
-
-          {state?.project?.didSpaceLastSyncedAt && (
-            <Typography variant="caption" color="#9CA3AF">
-              {/* @ts-ignore */}
-              <RelativeTime locale={locale} value={state.project.didSpaceLastSyncedAt} />
-            </Typography>
-          )}
-        </Stack>
       </Stack>
 
       <Stack direction="row" alignItems="center" gap={1}>
@@ -107,7 +72,6 @@ export default function DidSpacesSetting({ projectId }: { projectId: string }) {
           selected
           compat
           endpoint={session.user?.didSpace?.endpoint}
-          deps={[refreshSpaceCard]}
           action={(props) => <GatewayAction {...props} projectId={projectId} />}
         />
       </Stack>
