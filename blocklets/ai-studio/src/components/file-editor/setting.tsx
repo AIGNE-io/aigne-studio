288c7db40132c3c1432eeef61f8eb8a619391db6
donotlb <donotlb@gmail.com>
2024-12-05 16:17:51 +0800

    tweaks

--- blocklets/ai-studio/src/components/file-editor/setting.tsx
+++ blocklets/ai-studio/src/components/file-editor/setting.tsx
@@ -1,16 +1,13 @@
 import { useReadOnly } from '@app/contexts/session';
 import Close from '@app/pages/project/icons/close';
-import { useProjectStore } from '@app/pages/project/yjs-state';
 import { useLocaleContext } from '@arcblock/ux/lib/Locale/context';
 import {
-  defaultImageModel,
-  defaultTextModel,
-  getSupportedImagesModels,
-  getSupportedModels,
-} from '@blocklet/ai-runtime/common';
-import { AssistantYjs, isImageAssistant, isPromptAssistant, isRouterAssistant } from '@blocklet/ai-runtime/types';
-import { Icon } from '@iconify-icon/react';
-import AdjustmentsIcon from '@iconify-icons/tabler/adjustments';
+  AssistantYjs,
+  ModelBasedAssistantYjs,
+  isImageAssistant,
+  isPromptAssistant,
+  isRouterAssistant,
+} from '@blocklet/ai-runtime/types';
 import {
   Box,
   Button,
@@ -24,13 +21,12 @@ import {
   Typography,
 } from '@mui/material';
 import { bindDialog, usePopupState } from 'material-ui-popup-state/hooks';
-import { useMemo } from 'react';
-import { useAsync } from 'react-use';
 
+import { ModelSelectLite } from '../agent-models/model-select-lite';
+import { ModelSettingsMenuButton } from '../agent-models/model-settings';
+import { resolveModelType } from '../agent-models/utils';
 import Switch from '../custom/switch';
-import { brandIcon } from '../selector/model-select-field';
 import ImageSettings from './image-file/setting';
-import { AgentName } from './input/InputTable';
 import PromptSettings from './prompt-file/setting';
 
 export default function PromptSetting({
@@ -47,37 +43,6 @@ export default function PromptSetting({
   const { t } = useLocaleContext();
   const readOnly = useReadOnly({ ref: gitRef }) || disabled;
   const dialogState = usePopupState({ variant: 'dialog' });
-
-  const { projectSetting } = useProjectStore(projectId, gitRef);
-  const { value: supportedModels } = useAsync(async () => {
-    if (isPromptAssistant(value) || isRouterAssistant(value)) {
-      return getSupportedModels();
-    }
-
-    if (isImageAssistant(value)) {
-      return getSupportedImagesModels();
-    }
-
-    return [];
-  }, [value.type]);
-
-  const valueModel = isPromptAssistant(value) || isRouterAssistant(value) ? value.model : undefined;
-
-  const defaultModel = useMemo(() => {
-    if (isPromptAssistant(value) || isRouterAssistant(value)) {
-      return value?.model || projectSetting?.model || defaultTextModel;
-    }
-
-    if (isImageAssistant(value)) {
-      return value?.model || defaultImageModel;
-    }
-
-    return defaultTextModel;
-  }, [(value as any).model, projectSetting?.model]);
-
-  const modelDetail = useMemo(() => {
-    return supportedModels?.find((i) => i.model === defaultModel);
-  }, [defaultModel, supportedModels]);
 
   const conditionalBranch = value.type === 'router' && value.decisionType === 'json-logic';
   return (
@@ -97,46 +62,14 @@ export default function PromptSetting({
       )}
 
       {!conditionalBranch && (
-        <Stack
-          direction="row"
-          alignItems="center"
-          gap={0.5}
-          sx={{ cursor: 'pointer' }}
-          onClick={() => {
-            dialogState.open();
-          }}>
-          <Stack direction="row" alignItems="center" gap={0.5}>
-            {value.executor?.agent?.id ? (
-              <AgentName
-                type="llm-adapter"
-                showIcon
-                blockletDid={value.executor.agent.blockletDid}
-                projectId={value.executor.agent.projectId}
-                agentId={value.executor.agent.id}
-              />
-            ) : !valueModel && projectSetting.executor?.agent?.id ? (
-              <AgentName
-                type="llm-adapter"
-                showIcon
-                blockletDid={projectSetting.executor.agent.blockletDid}
-                projectId={projectSetting.executor.agent.projectId}
-                agentId={projectSetting.executor.agent.id}
-              />
-            ) : (
-              <>
-                {modelDetail && <Box className="center">{brandIcon(modelDetail!.brand)}</Box>}
-                <Typography variant="subtitle3" color="#030712" mt={-0.25}>
-                  {isPromptAssistant(value) || isRouterAssistant(value)
-                    ? modelDetail?.name || modelDetail?.model || modelDetail?.model
-                    : isImageAssistant(value)
-                      ? defaultModel
-                      : ''}
-                </Typography>
-              </>
-            )}
-          </Stack>
-
-          <Box component={Icon} icon={AdjustmentsIcon} sx={{ color: '#3B82F6', fontSize: 15 }} />
+        <Stack direction="row" alignItems="center">
+          <ModelSelectLite
+            type={resolveModelType(value.type)!}
+            projectId={projectId}
+            gitRef={gitRef}
+            agent={value as ModelBasedAssistantYjs}
+          />
+          <ModelSettingsMenuButton agent={value as ModelBasedAssistantYjs} />
         </Stack>
       )}
 
