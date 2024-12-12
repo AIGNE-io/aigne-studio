311e0afbc3950b0acabd2018d93a853af1ad0888
leermao <447552708@qq.com>
2024-12-12 18:59:54 +0800

    chore: 移除提示代码

--- packages/code-editor/src/plugins/close-tag.tsx
+++ packages/code-editor/src/plugins/close-tag.tsx
@@ -1,59 +1,6 @@
 import { Monaco } from '@monaco-editor/react';
-import * as MaterialUI from '@mui/material';
 
 import type { EditorInstance } from '../libs/type';
-
-const getAllMUIComponents = () => {
-  return Object.keys(MaterialUI).filter((key) => {
-    return /^[A-Z]/.test(key);
-  });
-};
-const muiComponents = getAllMUIComponents();
-
-let isProviderRegistered = false;
-const registerMUIComponentsAutocomplete = (monaco: Monaco) => {
-  if (isProviderRegistered) return;
-
-  monaco.languages.registerCompletionItemProvider('typescript', {
-    provideCompletionItems: (model, position) => {
-      const textUntilPosition = model.getValueInRange({
-        startLineNumber: position.lineNumber,
-        startColumn: 1,
-        endLineNumber: position.lineNumber,
-        endColumn: position.column,
-      });
-
-      const match = textUntilPosition.match(/<([A-Z][a-zA-Z]*)$/);
-      if (match) {
-        const word = match[1];
-        if (!word) return { suggestions: [] };
-
-        const range = {
-          startLineNumber: position.lineNumber,
-          endLineNumber: position.lineNumber,
-          startColumn: position.column - word.length,
-          endColumn: position.column,
-        };
-
-        return {
-          suggestions: muiComponents
-            .filter((comp) => comp.startsWith(word))
-            .map((comp) => ({
-              label: comp,
-              kind: monaco.languages.CompletionItemKind.Class,
-              insertText: comp,
-              range,
-              sortText: String.fromCharCode(100),
-            })),
-        };
-      }
-
-      return { suggestions: [] };
-    },
-  });
-
-  isProviderRegistered = true;
-};
 
 const useCloseTag = () => {
   const onMount = (editor: EditorInstance, monaco: Monaco) => {
@@ -93,8 +40,6 @@ const useCloseTag = () => {
 
       lastContent = currentLine;
     });
-
-    registerMUIComponentsAutocomplete(monaco);
   };
 
   return {
