c6ad39358a56157596fcbcf6d021154ca05b1a8e
JianChao <guhuo13@gmail.com>
2024-12-25 10:15:38 +0800

    fix: test error

--- scripts/global-teardown.ts
+++ scripts/global-teardown.ts
@@ -6,14 +6,15 @@ import { playwrightConfigAppNames } from '../tests/utils';
 
 const blockletCli = process.env.BLOCKLET_CLI || 'blocklet';
 
-const singleAppWallet = ensureWallet({ name: playwrightConfigAppNames.single, onlyFromCache: true });
-const multipleAppWallet = ensureWallet({ name: playwrightConfigAppNames.multiple, onlyFromCache: true });
 const ci = !!process.env.CI;
 
 export default async function globalTeardown() {
   if (ci) {
     return;
   }
+
+  const singleAppWallet = ensureWallet({ name: playwrightConfigAppNames.single, onlyFromCache: true });
+  const multipleAppWallet = ensureWallet({ name: playwrightConfigAppNames.multiple, onlyFromCache: true });
   // 其实本地才需要清理应用，CI 环境不需要
   await Promise.all([
     removeTestApp({ blockletCli, appSk: singleAppWallet.secretKey }),
