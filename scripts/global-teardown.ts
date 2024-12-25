33acf164f7fa6353f67b498b4cfdc45a7d21b265
JianChao <guhuo13@gmail.com>
2024-12-25 09:26:19 +0800

    fix: test error

--- scripts/global-teardown.ts
+++ scripts/global-teardown.ts
@@ -1,9 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies,no-console */
 import { removeTestApp } from '@blocklet/testlab/utils/server';
+import { ensureWallet } from '@blocklet/testlab/utils/wallet';
+
+import { playwrightConfigAppNames } from '../tests/utils';
 
 const blockletCli = process.env.BLOCKLET_CLI || 'blocklet';
 
-export default async function cleanupApps(singleAppWallet: any, multipleAppWallet: any) {
+const singleAppWallet = ensureWallet({ name: playwrightConfigAppNames.single, onlyFromCache: true });
+const multipleAppWallet = ensureWallet({ name: playwrightConfigAppNames.multiple, onlyFromCache: true });
+
+export default async function cleanupApps() {
   await Promise.all([
     removeTestApp({ blockletCli, appSk: singleAppWallet.secretKey }),
     removeTestApp({ blockletCli, appSk: multipleAppWallet.secretKey }),
