8b5eca4ee90c29c3f8edefa8bce959636ea76f4c
Yechao LI <liyechao@yechao.xyz>
2025-01-13 15:36:23 +0800

    fix: remove shiki dependency (#1657)
    * fix: remove shiki dependency
    
    * chore: bump version [skip blocklet]
    
    * lint error

--- packages/code-editor/src/test-editor.tsx
+++ packages/code-editor/src/test-editor.tsx
@@ -1,34 +1,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import Editor, { useMonaco } from '@monaco-editor/react';
-import { useEffect } from 'react';
-import { createHighlighter } from 'shiki';
+import Editor from '@monaco-editor/react';
 
-import { shikiToMonaco } from './libs/shiki-to-monaco';
-
-function ShikiMonacoEditor({ language = 'javascript', theme = 'red', ...props }: any) {
-  const monaco = useMonaco();
-
-  const handleEditorWillMount = (_editor: any, monaco: any) => {
-    createHighlighter({
-      themes: [theme],
-      langs: [language],
-    }).then((highlighter) => {
-      shikiToMonaco(highlighter, monaco);
-    });
-  };
-
-  useEffect(() => {
-    if (monaco && theme) {
-      createHighlighter({
-        themes: [theme],
-        langs: [language],
-      }).then((highlighter) => {
-        shikiToMonaco(highlighter, monaco);
-      });
-    }
-  }, [monaco, theme]);
-
-  return <Editor language={language} onMount={handleEditorWillMount} {...props} />;
+function TestCodeEditor({ language = 'javascript', ...props }: any) {
+  return <Editor language={language} {...props} />;
 }
 
-export default ShikiMonacoEditor;
+export default TestCodeEditor;
