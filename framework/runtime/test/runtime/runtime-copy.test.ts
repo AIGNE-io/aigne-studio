bbe36a675887f08e427b53522093096ceec2b1da
Yechao LI <liyechao@yechao.xyz>
2025-01-17 14:58:29 +0800

    fix: incorrect state/config for agent resolved from a copied runtime (#1665)
    * fix: function calls overrided by the latesst empty calls array
    
    * fix: incorrect state/config for agent resolved from a copied runtime
    
    * chore: support resolve an agent to renew its context
    
    * chore: bump version
    
    * chore: update deps
    
    * lint errors

--- framework/runtime/test/runtime/runtime-copy.test.ts
+++ framework/runtime/test/runtime/runtime-copy.test.ts
@@ -1,4 +1,4 @@
-import { OrderedRecord } from '@aigne/core';
+import { LocalFunctionAgent, OrderedRecord, TYPES } from '@aigne/core';
 import { expect, test } from 'bun:test';
 
 import { Runtime } from '../../src';
@@ -29,8 +29,6 @@ test('Runtime.copy', async () => {
     'id',
     'name',
     'config',
-    'agents',
-    'container' as keyof typeof runtime,
     'runnables' as keyof typeof runtime,
     'runnableDefinitions' as keyof typeof runtime,
   ];
@@ -38,4 +36,65 @@ test('Runtime.copy', async () => {
   for (const member of strictEqualMembers) {
     expect(copy[member]).toBe(runtime[member]!);
   }
+
+  expect(runtime.state).toEqual({});
+  expect(copy.state).toEqual({ userId: 'foo' });
+});
+
+test('Runtime.copy.resolve should resolve with new state and config', async () => {
+  const runtime = new Runtime({
+    id: 'test',
+    name: 'test',
+    projectDefinition: {
+      id: 'test',
+      name: 'test',
+      runnables: OrderedRecord.fromArray([]),
+    },
+    state: {},
+    config: {},
+  });
+
+  const agent = LocalFunctionAgent.create({
+    context: runtime,
+    name: 'test',
+    inputs: {},
+    outputs: {
+      state: {
+        type: 'object',
+      },
+      config: {
+        type: 'object',
+      },
+    },
+    function: async (_, { context }) => {
+      return { state: context.state, config: context.config };
+    },
+  });
+
+  expect(await agent.run({})).toEqual({ state: {}, config: {} });
+  expect(await runtime.resolve(agent.id).then((a) => a.run({}))).toEqual({ state: {}, config: {} });
+
+  const copy = runtime.copy({
+    state: { userId: 'foo' },
+    config: {
+      llmModel: {
+        default: {
+          model: 'gemini-2.0-pro',
+        },
+      },
+    },
+  });
+
+  expect(copy.resolveDependency<Runtime>(TYPES.context)).toBe(copy);
+
+  expect(await copy.resolve(agent.id).then((a) => a.run({}))).toEqual({
+    state: { userId: 'foo' },
+    config: {
+      llmModel: {
+        default: {
+          model: 'gemini-2.0-pro',
+        },
+      },
+    },
+  });
 });
